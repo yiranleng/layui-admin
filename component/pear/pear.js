c9668f03474a8739a3bfd4f4523d8de3be5ab2c9
就眠儀式 <854085467@qq.com>
2023-01-12 11:16:23 +0800

    🔥 加入 translate 国家化

--- component/pear/pear.js
+++ component/pear/pear.js
@@ -45,70 +45,12 @@ layui.config({
 	watermark:"watermark/watermark", //水印
 	fullscreen:"fullscreen",  //全屏组件
 	popover:"popover/popover", //汽泡组件
-	translate:"translate"	//多语言翻译组件
+	translate:"translate"	// 翻译组件
 }).use(['layer', 'theme', 'translate'], function () {
+	
+	// 全局主题
 	layui.theme.changeTheme(window, false);
 	
-	/***** translate.js 翻译 ******/
-	var template_temp_pearInterval = setInterval(function(){
-		if(typeof(parent.window.pearTranslateConfig) == 'undefined'){
-			//配置还没加载出来，等待加载
-			return;
-		}
-		//admin.js 初始化完毕，translate配置已获取成功
-		var translateConfig = parent.window.pearTranslateConfig;
-		//停止
-		clearInterval(template_temp_pearInterval);
-		
-		/***** 配置项赋予 *****/
-		if(typeof(translateConfig.autoDiscriminateLocalLanguage) != 'undefined' && (translateConfig.autoDiscriminateLocalLanguage == true || translateConfig.autoDiscriminateLocalLanguage == 'true' )){
-			translate.setAutoDiscriminateLocalLanguage();	//设置用户第一次用时，自动识别其所在国家的语种进行切换
-		}
-		if(typeof(translateConfig.currentLanguage) != 'undefined' && translateConfig.currentLanguage.length > 0){
-			translate.language.setLocal(translateConfig.currentLanguage);
-		}
-		if(typeof(translateConfig.ignoreClass) != 'undefined' && translateConfig.ignoreClass.length > 0){
-			var classs = translateConfig.ignoreClass.split(',');
-			for(var ci = 0; ci < classs.length; ci++){
-				var className = classs[ci].trim();
-				if(className.length > 0){
-					if(translate.ignore.class.indexOf(className.toLowerCase()) > -1){
-						//已经有了，忽略
-					}else{
-						//还没有，加入
-						translate.ignore.class.push(className);
-					}
-				}
-			}
-		}
-		if(typeof(translateConfig.ignoreTag) != 'undefined' && translateConfig.ignoreTag.length > 0){
-			var tags = translateConfig.ignoreTag.split(',');
-			for(var ti = 0; ti < tags.length; ti++){
-				var tagName = tags[ti].trim();
-				if(tagName.length > 0){
-					if(translate.ignore.tag.indexOf(tagName.toLowerCase()) > -1){
-						//已经有了，忽略
-					}else{
-						//还没有，加入
-						translate.ignore.tag.push(tagName);
-					}
-				}
-			}
-		}
-		//开启html页面变化的监控，对变化部分会进行自动翻译。注意，这里变化部分，是指当 translate.execute(); 已经完全执行完毕之后，如果页面再有变化的部分，才会对其进行翻译。
-		translate.listener.start();	
-		//页面加载完毕后执行翻译
-		if(document.readyState == 'complete'){
-			translate.execute();
-		}else{
-			window.onload = function(){
-				translate.execute();
-			}
-		}
-		//避免有遗漏，特别是表格的render渲染等，诡异的会复原
-		setTimeout(translate.execute,1500);
-		
-    }, 30);
-	/***** translate.js 翻译 结束 ******/
-	
+	// 全局翻译
+	layui.translate.translate();
 });
